3f5aca04bc945a376a8cf75893b85d93ad0cb034
rashkov7 <grashcov@gmail.com>
2022-04-13 14:24:15 +0300

    settings -> host added

--- gym/settings.py
+++ gym/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-c3+qj54!4y*%_du%c77tgew)ss%(r@f&6-mjhn8hu6(s9&tgmn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', " gym-corner.herokuapp.com" ]
+ALLOWED_HOSTS = ['127.0.0.1', "gym-corner.herokuapp.com"]
 
 
 # Application definition
